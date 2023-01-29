1da6206e681ac35bad71a613b91fe3e7bd133bb7
Axel <axel.valentinbvs@yahoo.com>
2023-01-29 17:26:13 +0000

    add task

--- scripts.js
+++ scripts.js
@@ -108,19 +108,129 @@ $(document).ready(function () {
     },
   });
 
-  // Latest videos section [Home page]
-  $("#sort-by, #topic, #search").on("change", function () {
-    $(".loader").show();
-    let sort = $("#sort-by").val();
-    let topic = $("#topic").val();
-    let q = $("#search").val();
-    $.ajax({
-      url: "https://smileschool-api.hbtn.info/courses",
-      type: "GET",
-      data: { q: q, topic: topic, sort: sort },
-      success: function (data) {
-        $(".loader").hide();
-      },
-    });
+  // Latest videos section
+  $(".loader").show();
+  $.ajax({
+    url: "https://smileschool-api.hbtn.info/latest-videos",
+    type: "GET",
+    success: function (data) {
+      $(".loader").hide();
+
+      for (let i = 0; i < data.length; i++) {
+        let video = data[i];
+
+        let star_on = `<img src="images/star_on.png" width="15" height="15" alt="Star on" loading="lazy" />`;
+        let star_off = `<img src="images/star_off.png" width="15" height="15" alt="Star off" loading="lazy" />`;
+        let stars = "";
+        for (let j = 0; j < 5; j++) {
+          if (j < video.star) {
+            stars += star_on;
+          } else {
+            stars += star_off;
+          }
+        }
+        video.star = stars;
+
+        let Latest_videosHTML = `<div class="carousel-item">
+         <div class="row justify-content-center">
+           <div class="mx-1">
+             <div class="card mx-auto my-3">
+               <img class="card-img-top" src="${video.thumb_url}" alt="Tuto" />
+               <img class="play-icon" src="images/play.png" alt="play" />
+               <div class="card-body">
+                 <p class="font-weight-bold black-text">
+                   ${video.title}<br />
+                   <span class="text-secondary"
+                     >${video.subtitle}</span
+                   >
+                 </p>
+                 <div class="row justify-content-start">
+                   <div class="col-2 pr-0">
+                     <img
+                       class="rounded-circle"
+                       src="${video.author_pic_url}"
+                       width="30"
+                       height="30"
+                       alt="Profile 1"
+                       loading="lazy"
+                     />
+                   </div>
+                   <div class="col mt-1 pl-0 purple-text font-weight-bold">
+                     ${video.author}
+                   </div>
+                 </div>
+       
+                 <div class="row justify-content-between mt-2">
+                   <div class="col">
+                     ${video.star}
+                   </div>
+                   <div class="col-4 text-right font-weight-bold purple-text">
+                     ${video.duration}
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>`;
+        $("#Latest-videos-section").append(Latest_videosHTML);
+      }
+      $("#Latest-videos-section .carousel-item:first-child").addClass("active");
+    },
+  });
+
+  // courses section
+  $(".loader").show();
+  $.ajax({
+    url: "https://smileschool-api.hbtn.info/courses",
+    type: "GET",
+    data: {
+      q: searchValue,
+      topic: topicValue,
+      sort: sortValue,
+    },
+    success: function (data) {
+      $(".loader").hide();
+
+      for (let i = 0; i < data.length; i++) {
+        let course = data[i];
+
+        let coursesHTML = `
+        <div class="form-group col-md-12 col-lg-3">
+          <label for="search-input">Keywords</label>
+          <input
+            class="form-control opacity-courses-section"
+            id="search-input"
+            type="text"
+            placeholder=" Search by Keywords"
+            aria-label="Search"
+          />
+        </div>
+        <div class="form-group col-lg-3 col-md-6">
+          <label for="select-input">Topic</label>
+          <select
+            class="form-control opacity-courses-section"
+            id="pack-select"
+          >
+            <option class="text-dark font-weight-bold">Novice</option>
+            <option class="text-dark font-weight-bold">Intermediate</option>
+            <option class="text-dark font-weight-bold">Expert</option>
+          </select>
+        </div>
+        <div class="form-group col-lg-3 col-md-6">
+          <label for="select-input">Sort by</label>
+          <select
+            class="form-control opacity-courses-section"
+            id="pack-select"
+          >
+            <option class="text-dark font-weight-bold">Most Popular</option>
+            <option class="text-dark font-weight-bold">Most Recent</option>
+            <option class="text-dark font-weight-bold">Most Viewed</option>
+          </select>
+        </div>
+      `;
+        $("#form").append(coursesHTML);
+      }
+    },
   });
 });
