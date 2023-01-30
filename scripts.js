2c1dc3cada4f3ab0f50048cfe4229906360997e8
Axel <axel.valentinbvs@yahoo.com>
2023-01-30 13:17:38 +0000

    do not work

--- scripts.js
+++ scripts.js
@@ -1,45 +1,49 @@
-$(document).ready(function () {
-  // quotes section [Home page, Pricing page]
+// quotes section [Home page, Pricing page]
+function quotes() {
   $(".loader").show();
   $.ajax({
     url: "https://smileschool-api.hbtn.info/quotes",
     type: "GET",
+    dataType: "json",
     success: function (data) {
       $(".loader").hide();
 
       for (let i = 0; i < data.length; i++) {
         let quote = data[i];
 
-        let quoteHTML = `<div class="carousel-item carousel-item-content">
-                              <div class="row">
-                                <div class="col-sm-3 text-center">
-                                  <img
-                                    class="rounded-circle"
-                                    src="${quote.pic_url}"
-                                    class="d-block w-100"
-                                    alt="random person image"
-                                  />
-                                </div>
-                                <div class="col-sm-8 ml-3 d-flex flex-column">
-                                  <div>
-                                    ${quote.text}
-                                  </div>
-                                  <div class="font-weight-bold mt-3">${quote.name}</div>
-                                  <div>${quote.title}</div>
-                                </div>
-                              </div>
-                            </div>`;
+        let quoteHTML = `
+        <div class="carousel-item carousel-item-content">
+          <div class="row">
+            <div class="col-sm-3 text-center">
+              <img
+                class="rounded-circle"
+                src="${quote.pic_url}"
+                class="d-block w-100"
+                alt="random person image"
+              />
+            </div>
+            <div class="col-sm-8 ml-3 d-flex flex-column">
+              <div>${quote.text}</div>
+              <div class="font-weight-bold mt-3">${quote.name}</div>
+              <div>${quote.title}</div>
+            </div>
+          </div>
+        </div>
+        `;
         $("#quotes-section").append(quoteHTML);
         $("#quotes-section .carousel-item:first-child").addClass("active");
       }
     },
   });
+}
 
-  // video section [Home page]
+//video section [Home page]
+function most_popular_video() {
   $(".loader").show();
   $.ajax({
     url: "https://smileschool-api.hbtn.info/popular-tutorials",
     type: "GET",
+    dataType: "json",
     success: function (data) {
       $(".loader").hide();
 
@@ -107,11 +111,14 @@ $(document).ready(function () {
       $("#video-section .carousel-item:first-child").addClass("active");
     },
   });
+}
 
-  // Latest videos section
+// Latest videos section [Home page]
+function latest_video() {
   $(".loader").show();
   $.ajax({
     url: "https://smileschool-api.hbtn.info/latest-videos",
+    dataType: "json",
     type: "GET",
     success: function (data) {
       $(".loader").hide();
@@ -178,59 +185,160 @@ $(document).ready(function () {
       $("#Latest-videos-section .carousel-item:first-child").addClass("active");
     },
   });
+}
 
-  // courses section
+// Courses section [courses page]
+function courses() {
+  let topicSelect = $("#topic-select");
+  let sortSelect = $("#sort-select");
+  let searchInput = $("#search-input");
+
   $(".loader").show();
   $.ajax({
     url: "https://smileschool-api.hbtn.info/courses",
     type: "GET",
+    dataType: "json",
     data: {
-      q: searchValue,
-      topic: topicValue,
-      sort: sortValue,
+      q: searchInput.val(),
+      topic: topicSelect.val(),
+      sort: sortSelect.val(),
     },
+
     success: function (data) {
       $(".loader").hide();
+      // seach input
+      $(searchInput).on("keyup", function () {
+        updateCards(
+          data,
+          searchInput.val(),
+          topicSelect.val(),
+          sortSelect.val()
+        );
+      });
 
-      for (let i = 0; i < data.length; i++) {
-        let course = data[i];
+      // topics select
+      topicSelect.on("change", function () {
+        updateCards(
+          data,
+          searchInput.val(),
+          topicSelect.val(),
+          sortSelect.val()
+        );
+      });
 
-        let coursesHTML = `
-        <div class="form-group col-md-12 col-lg-3">
-          <label for="search-input">Keywords</label>
-          <input
-            class="form-control opacity-courses-section"
-            id="search-input"
-            type="text"
-            placeholder=" Search by Keywords"
-            aria-label="Search"
-          />
-        </div>
-        <div class="form-group col-lg-3 col-md-6">
-          <label for="select-input">Topic</label>
-          <select
-            class="form-control opacity-courses-section"
-            id="pack-select"
-          >
-            <option class="text-dark font-weight-bold">Novice</option>
-            <option class="text-dark font-weight-bold">Intermediate</option>
-            <option class="text-dark font-weight-bold">Expert</option>
-          </select>
-        </div>
-        <div class="form-group col-lg-3 col-md-6">
-          <label for="select-input">Sort by</label>
-          <select
-            class="form-control opacity-courses-section"
-            id="pack-select"
-          >
-            <option class="text-dark font-weight-bold">Most Popular</option>
-            <option class="text-dark font-weight-bold">Most Recent</option>
-            <option class="text-dark font-weight-bold">Most Viewed</option>
-          </select>
-        </div>
-      `;
-        $("#form").append(coursesHTML);
+      // sort select
+      sortSelect.on("change", function () {
+        updateCards(
+          data,
+          searchInput.val(),
+          topicSelect.val(),
+          sortSelect.val()
+        );
+      });
+
+      // Add topics to select
+      $.each(data.topics, function (index, topic) {
+        topicSelect.append(
+          '<option class="text-dark font-weight-bold" value="' +
+            topic +
+            '">' +
+            topic +
+            "</option>"
+        );
+      });
+
+      // Add sort options to select
+      $.each(data.sorts, function (index, sort) {
+        sortSelect.append(
+          '<option class="text-dark font-weight-bold" value="' +
+            sort +
+            '">' +
+            sort +
+            "</option>"
+        );
+      });
+
+      function updateCards(data, search, topic, sort) {
+        let filteredData = data;
+
+        // Filter based on search input
+        if (search) {
+          filteredData = filteredData.filter(function (item) {
+            return item.title.toLowerCase().includes(search.toLowerCase());
+          });
+        }
+
+        // Filter based on topic select
+        if (topic && topic !== "All topics") {
+          filteredData = filteredData.filter(function (item) {
+            return item.topic === topic;
+          });
+        }
+
+        // Sort based on sort select
+        if (sort === "title") {
+          filteredData.sort(function (a, b) {
+            return a.title.localeCompare(b.title);
+          });
+        } else if (sort === "duration") {
+          filteredData.sort(function (a, b) {
+            return a.duration - b.duration;
+          });
+        }
+
+        // Clear existing cards
+        $("#card-video").empty();
+
+        for (let i = 0; i < filteredData.length; i++) {
+          let video = filteredData[i];
+
+          let star_on = `<img src="images/star_on.png" width="15" height="15" alt="Star on" loading="lazy" />`;
+          let star_off = `<img src="images/star_off.png" width="15" height="15" alt="Star off" loading="lazy" />`;
+          let stars = "";
+          for (let j = 0; j < 5; j++) {
+            if (j < video.star) {
+              stars += star_on;
+            } else {
+              stars += star_off;
+            }
+          }
+          video.star = stars;
+
+          let cardHTML = `
+                <div class="card mx-auto my-3">
+                <img class="card-img-top" src="${video.thumb_url}" alt="Tuto" />
+                <img class="play-icon" src="images/play.png" alt="play" />
+                <div class="card-body">
+                  <p class="font-weight-bold black-text">
+                    ${video.title}<br />
+                    <span class="text-secondary">${video.subtitle}</span>
+                  </p>
+                  <div class="row justify-content-start">
+                    <div class="col-2 pr-0">
+                      <img src="${video.avatar_url}" alt="Avatar" class="avatar" />
+                    </div>
+                    <div class="col-10 pl-0">
+                      <p class="text-secondary">${video.author}</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer d-flex justify-content-between align-items-center">
+                  <p>${video.duration} min</p>
+                  <p>${video.star}</p>
+                </div>
+                </div>
+          `;
+          $("#card-video").append(cardHTML);
+        }
+        $("#card-video").addClass("active");
       }
     },
   });
+}
+
+$(document).ready(function () {
+  quotes();
+  most_popular_video();
+  latest_video();
+  courses();
 });
